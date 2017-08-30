3b6bf1ff5528db3f415f701387cb361df27e8d80
James Sullivan <jamesfsullivan5@gmail.com>
2017-08-29 22:57:46 -0600

    Add working tabs.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 
+
+$('.tab').click((event) => {
+  const $newActiveTab = event.target
+    .closest('.tab');
+  const dataTab = $($newActiveTab).data()
+
+  $('.tab').removeClass('tab-active');
+  $($newActiveTab).addClass('tab-active');
+
+  $('.section-body').each((index, element) => {
+    if ($(element).data().tab === dataTab.tab) {
+      $(element).removeClass('hidden');
+    } else {
+      $(element).addClass('hidden');
+    }
+  })
+})
